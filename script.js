237c10bf4109721c825a98daebe28646986414fe
Mighty-Nievl <rezalhbramantara@gmail.com>
2025-09-06 12:27:42 +0700

    Changes to be committed: 	modified:   custom.css 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const menuBtn = document.getElementById("menuBtn");
-const navbar = document.getElementById("navbar");
+// const menuBtn = document.getElementById("menuBtn");
+// const navbar = document.getElementById("navbar");
 
-menuBtn.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-    // toggle class "active"
+// menuBtn.addEventListener("click", () => {
+//     navbar.classList.toggle("active");
+//     // toggle class "active"
+// });
+
+
+const navbar = document.querySelector('.navbar-nav');
+const collapse = document.getElementById('navbarNav');
+
+collapse.addEventListener('show.bs.collapse', () => {
+    navbar.classList.add('bg-secondary-subtle');
+});
+
+collapse.addEventListener('hide.bs.collapse', () => {
+    navbar.classList.remove('bg-secondary-subtle');
 });
